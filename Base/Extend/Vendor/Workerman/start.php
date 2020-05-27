869a3319a5084a19e73faa2df4e0499acf2fc2f8
value94 <x_x9403@163.com>
2020-05-27 14:01:57 +0800

    fix require

--- Base/Extend/Vendor/Workerman/start.php
+++ Base/Extend/Vendor/Workerman/start.php
@@ -26,10 +26,10 @@ if(!extension_loaded('posix'))
 // 标记是全局启动
 define('GLOBAL_START', 1);
 
-include_once('vendor/autoload.php');
+require_once __DIR__ . '/vendor/autoload.php';
 
 // 加载所有Applications/*/start.php，以便启动所有服务
-foreach(glob('Applications/*/start*.php') as $start_file)
+foreach(glob(__DIR__.'/Applications/*/start*.php') as $start_file)
 {
     require_once $start_file;
 }
