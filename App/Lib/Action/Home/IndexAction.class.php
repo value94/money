97e6f54729c01ea13eeb53c09cf93127145c222c
value94 <x_x9403@163.com>
2020-05-31 17:42:30 +0800

    add index page

--- App/Lib/Action/Home/IndexAction.class.php
+++ App/Lib/Action/Home/IndexAction.class.php
@@ -24,6 +24,28 @@ class IndexAction extends CommonAction
 		$this->user = $_var_7;
 		$this->display();
 	}
+
+    public function index_order()
+    {
+        //    sendTsms('15925748132','通知：订单提现成功，您的订单未完成。具体详情，请登录平台或联系业务员咨询');exit;
+        //随机生成一批jk成功的
+        $_var_0 = '13,15,17,18';
+        $_var_1 = explode(',', $_var_0);
+        $_var_2 = array();
+        for ($_var_3 = 0; $_var_3 < 30; $_var_3++) {
+            $_var_4 = rand(0, count($_var_1) - 1);
+            $_var_5 = $_var_1[$_var_4] . rand(0, 9) . '****' . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9);
+            $_var_6 = rand(100, 2000) * 100;
+            $_var_2[] = array('phone' => $_var_5, 'money' => $_var_6);
+        }
+        $this->redaydata = $_var_2;
+        $_var_7 = $this->getLoginUser();
+        if (!$_var_7) {
+            $_var_7 = 0;
+        }
+        $this->user = $_var_7;
+        $this->display();
+    }
 	public function money()
 	{
 		//判断是否已登录
