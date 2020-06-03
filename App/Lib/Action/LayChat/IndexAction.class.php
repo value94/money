5cb7a294c4eed187c3b76f8913b317ac60799ff2
value94 <x_x9403@163.com>
2020-06-03 17:48:01 +0800

    finish mobile laychat

--- App/Lib/Action/LayChat/IndexAction.class.php
+++ App/Lib/Action/LayChat/IndexAction.class.php
@@ -37,6 +37,92 @@ class IndexAction extends Action
         if ($mine['customer_id']) {
             $customer_data = M('richat_chatuser')->where("id={$mine['customer_id']}")->find();
         }
+        //查询自己的信息
+        $mine = M('richat_chatuser')->where(['id' => cookie('uid')])->find();
+
+        // 客服只能看到自己绑定的用户
+        if ($mine['username'] != 'admin') {
+            $other = M('richat_chatuser')->where("customer_id={$mine['id']} or user_type=1")->select();
+        }
+
+        // 被绑定客户只能看到自己的客服
+        if ($mine['customer_id'] != null) {
+            $other = M('richat_chatuser')->where("id={$mine['customer_id']}")->select();
+        }
+
+        //查询当前用户的所处的群组
+        $groupArr = [];
+        $groups = M('richat_groupdetail')->field('groupid')->where(['userid' => cookie('uid')])->group('groupid')->select();
+
+        if (!empty($groups)) {
+            foreach ($groups as $key => $vo) {
+                $ret = M('richat_chatgroup')->where(['id' => $vo['groupid']])->find();
+                if (!empty($ret)) {
+                    $groupArr[] = $ret;
+                }
+            }
+        }
+        unset($ret, $groups);
+
+        $online = 0;
+        $group = [];  //记录分组信息
+        $userGroup = C('user_group');
+        $list = [];  //群组成员信息
+        $i = 0;
+        $j = 0;
+        foreach ($userGroup as $key => $vo) {
+            $group[$i] = [
+                'groupname' => $vo,
+                'id' => $key,
+                'online' => 0,
+                'list' => []
+            ];
+            $i++;
+        }
+        unset($userGroup);
+
+        foreach ($group as $key => $vo) {
+
+            foreach ($other as $k => $v) {
+
+                if ($vo['id'] == $v['groupid']) {
+
+                    $list[$j]['username'] = $v['username'];
+                    $list[$j]['id'] = $v['id'];
+                    $list[$j]['avatar'] = $v['avatar'];
+                    $list[$j]['sign'] = $v['sign'];
+
+                    if ('online' == $v['status']) {
+                        $online++;
+                    }
+
+                    $group[$key]['online'] = $online;
+                    $group[$key]['list'] = $list;
+
+                    $j++;
+                }
+            }
+            $j = 0;
+            $online = 0;
+            unset($list);
+        }
+        //print_r($group);die;
+        unset($other);
+
+        $data = [
+            'mine' => [
+                'username' => $mine['username'],
+                'id' => $mine['id'],
+                'status' => 'online',
+                'sign' => $mine['sign'],
+                'avatar' => $mine['avatar']
+            ],
+            'friend' => $group,
+            'group' => $groupArr
+
+        ];
+        $this->group = json_encode($group);
+//        dump($this->group);die();
 
         $this->uinfo = $mine;
         $this->customer_data = $customer_data;
