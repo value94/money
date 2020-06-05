cbac65f6af20cd76349713a4e9461b0809f1b6d9
value94 <x_x9403@163.com>
2020-06-05 21:22:29 +0800

    add worker man ssl

--- Base/Extend/Vendor/Workerman/Applications/Chat/start_gateway.php
+++ Base/Extend/Vendor/Workerman/Applications/Chat/start_gateway.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * This file is part of workerman.
  *
@@ -11,14 +11,24 @@
  * @link http://www.workerman.net/
  * @license http://www.opensource.org/licenses/mit-license.php MIT License
  */
+
 use \Workerman\Worker;
 use \GatewayWorker\Gateway;
 use \Workerman\Autoloader;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
+$context = array(
+    'ssl' => array(
+        'local_cert' => '/www/server/panel/vhost/cert/45.137.10.224/fullchain.pem', // 也可以是crt文件
+        'local_pk' => '/www/server/panel/vhost/cert/45.137.10.224/privkey.pem',
+        'verify_peer' => false,
+    )
+);
 // gateway 进程
-$gateway = new Gateway("Websocket://0.0.0.0:8282");
+$gateway = new Gateway("Websocket://0.0.0.0:8282", $context);
+// 设置https
+$gateway->transport = 'ssl';
 // 设置名称，方便status时查看
 $gateway->name = 'ChatGateway';
 // 设置进程数，gateway进程数建议与cpu核数相同
@@ -26,7 +36,10 @@ $gateway->count = 1;
 // 分布式部署时请设置成内网ip（非127.0.0.1）
 $gateway->lanIp = '127.0.0.1';
 // 内部通讯起始端口。假如$gateway->count=4，起始端口为2300
-// 则一般会使用2300 2301 2302 2303 4个端口作为内部通讯端口 
+// 则一般会使用2300 2301 2302 2303 4个端口作为内部通讯端口
+// 证书最好是申请的证书
+
+
 $gateway->startPort = 2300;
 // 心跳间隔
 $gateway->pingInterval = 10;
@@ -54,8 +67,7 @@ $gateway->onConnect = function($connection)
 */
 
 // 如果不是在根目录启动，则运行runAll方法
-if(!defined('GLOBAL_START'))
-{
+if (!defined('GLOBAL_START')) {
     Worker::runAll();
 }
 
