18d6ebd108dcbffe708c00e1163b44641a743067
value94 <x_x9403@163.com>
2020-06-12 19:58:37 +0800

    fix hetong page

--- App/Lib/Action/Home/OrderAction.class.php
+++ App/Lib/Action/Home/OrderAction.class.php
@@ -216,7 +216,7 @@ class OrderAction extends CommonAction
 
         // 如果小数部分处理完之后是00，需要处理下
         $dec = rtrim($dec, '0');
-        var_dump($dec);
+//        var_dump($dec);
         // 小数部分从左向右找
         if (!empty ($dec)) {
             $res .= $int_unit;
